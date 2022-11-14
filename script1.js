e7f905f908e4e8826f0507db7a970c82ff01072e
William Amo <willbandeiras@gmail.com.br>
2022-11-14 20:01:31 -0300

    test

---
+++ script1.js
@@ -0,0 +1,142 @@
+
+ let total=0
+ 
+function q1(){
+    const button1 = document.getElementById("1");
+       if(button1.checked == true){
+                (total++)}}
+    
+function q2(){
+        const button3 = document.getElementById("8");
+        if(button3.checked == true){
+            (total++)}
+}
+function q3(){
+        
+        const button2 = document.getElementById("12");
+        if(button2.checked == true){
+            (total++)}    
+}
+
+function q4(){
+        const button5 = document.getElementById("20");
+        if(button5.checked == true){
+            (total++)}
+    }
+
+function q5(){
+        const button1 = document.getElementById("21");
+        if(button1.checked == true){
+            (total++)}
+}
+
+function q6(){
+    const button1 = document.getElementById("30");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q7(){
+    const button1 = document.getElementById("33");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q8(){
+    const button1 = document.getElementById("36");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q9(){
+    const button1 = document.getElementById("42");
+    if(button1.checked == true){
+        (total++)}
+}
+function q10(){
+    const button1 = document.getElementById("48");
+    if(button1.checked == true){
+        (total++)}
+}
+function q11(){
+    const button1 = document.getElementById("51");
+       if(button1.checked == true){
+                (total++)}}
+    
+function q12(){
+        const button3 = document.getElementById("57");
+        if(button3.checked == true){
+            (total++)}
+}
+function q13(){
+        
+        const button2 = document.getElementById("62");
+        if(button2.checked == true){
+            (total++)}    
+}
+
+function q14(){
+        const button5 = document.getElementById("66");
+        if(button5.checked == true){
+            (total++)}
+    }
+
+function q15(){
+        const button1 = document.getElementById("72");
+        if(button1.checked == true){
+            (total++)}
+}
+
+function q16(){
+    const button1 = document.getElementById("79");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q17(){
+    const button1 = document.getElementById("81");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q18(){
+    const button1 = document.getElementById("87");
+    if(button1.checked == true){
+        (total++)}
+}
+
+function q19(){
+    const button1 = document.getElementById("94");
+    if(button1.checked == true){
+        (total++)}
+}
+function q20(){
+    const button1 = document.getElementById("96");
+    if(button1.checked == true){
+        (total++)}
+}
+    
+function ALL(){
+    q1()
+    q2()
+    q3()
+    q4()
+    q5()
+    q6()
+    q7()
+    q8()
+    q9()
+    q10()   
+    console.log(total) 
+
+    alert('você fez um total de : ' + (1000/20) * total  +
+    ' pontos neste simulado!')
+    //location.href = "resultado.html";
+    }
+   
+   
+
+
+     
+      
+
